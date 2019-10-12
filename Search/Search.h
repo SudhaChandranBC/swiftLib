5c1b0dfb6b30e8f0e06931c230ff304fb6b94163
Chandran, Sudha <sudhachandran1bc@gmail.com>
2019-10-12 17:45:59 +0530

    Initial Commit

---
+++ Search/Search.h
@@ -0,0 +1,19 @@
+//
+//  Search.h
+//  Search
+//
+//  Created by Chandran, Sudha | SDTD on 12/10/19.
+//  Copyright © 2019 Chandran, Sudha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Search.
+FOUNDATION_EXPORT double SearchVersionNumber;
+
+//! Project version string for Search.
+FOUNDATION_EXPORT const unsigned char SearchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Search/PublicHeader.h>
+
+
