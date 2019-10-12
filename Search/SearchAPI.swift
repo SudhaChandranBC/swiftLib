e2da73e41da83d068571c862f4850d9eceac498f
Chandran, Sudha <sudhachandran1bc@gmail.com>
2019-10-12 18:09:08 +0530

    Added test cases

---
+++ Search/SearchAPI.swift
@@ -0,0 +1,23 @@
+//
+//  File.swift
+//  Search
+//
+//  Created by Chandran, Sudha | SDTD on 12/10/19.
+//  Copyright © 2019 Chandran, Sudha. All rights reserved.
+//
+
+import Foundation
+
+public final class SearchAPI {
+    
+    let name = "SearchAPI"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
