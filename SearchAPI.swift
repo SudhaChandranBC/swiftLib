56cacb647a92409cd6e6d40a1cc61fc547cfbe88
Chandran, Sudha <sudhachandran1bc@gmail.com>
2019-10-12 17:56:13 +0530

    first commit

---
+++ SearchAPI.swift
@@ -0,0 +1,13 @@
+//
+//  SearchAPI.swift
+//  Search
+//
+//  Created by Chandran, Sudha | SDTD on 12/10/19.
+//  Copyright © 2019 Chandran, Sudha. All rights reserved.
+//
+
+import UIKit
+
+class SearchAPI: NSObject {
+
+}
---
+++ SearchExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SearchExample
+//
+//  Created by Chandran, Sudha | SDTD on 12/10/19.
+//  Copyright © 2019 Chandran, Sudha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
